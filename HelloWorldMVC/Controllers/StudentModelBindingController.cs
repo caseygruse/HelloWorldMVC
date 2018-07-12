d450333d7dba1ca4c7703198d7135a5d11b367df
caseygruse.222 <caseygruse.222@gmail.com>
2018-07-12 10:55:48 -0700

    added data binding

---
+++ HelloWorldMVC/Controllers/StudentModelBindingController.cs
@@ -0,0 +1,32 @@
+﻿using HelloWorldMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorldMVC.Controllers
+{
+    public class StudentModelBindingController : Controller
+    {
+        // GET: StudentModelBinding/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Models.Student stu)
+        {
+            //this ensures all validation rules for the model have passed.
+            if (ModelState.IsValid)
+            {
+                if (StudentDB.AddStudent(stu))
+                {
+                    ViewBag.Added = true;
+                }
+            }
+
+            return View();
+        }
+    }
+}
--- HelloWorldMVC/Models/Student.cs
+++ HelloWorldMVC/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,13 +8,28 @@ namespace HelloWorldMVC.Models
 {
     public class Student
     {
+        [Required]// makes sure there is something there.
+        [RegularExpression(@"^\d{9}$", ErrorMessage = "SID must be 9 digits")]
         public string StudentId { get; set; }
 
+        [Required(ErrorMessage = "Give me your name")]// makes sure there is something there.
+        [Display(Name ="First Name ")]
+        [StringLength(25, ErrorMessage = "This must be 1 to 25 characters")]
         public string FirstName { get; set; }
-
+        
+        [Required]// makes sure there is something there.
+        [Display(Name = "Last Name ")]
         public string LastName { get; set; }
 
+        // value types such as DateOfBirth are always required because they have to have a value.
         public DateTime DateOfBirth { get; set; }
+
+        //Note: we are not implementing this but this will validate 
+        //an email to the fullest.
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+
 
     }
 }
